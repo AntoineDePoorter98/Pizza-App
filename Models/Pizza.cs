e8a9dd47ea6f8b2ae81b1333dc852dbf7f92fb32
Antoine De Poorter <antoine.depoorter@outlook.com>
2022-06-21 17:34:29 +0200

    first commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿namespace pizza_app.Models
+{
+    public class Pizza
+    {
+        public int PizzaID { get; set; }
+        public string nom { get; set; }
+        public float prix { get; set; }
+        public bool vegetarienne { get; set; }
+        public string ingredients { get; set; }
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using pizza_app.Data;
+using pizza_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pizza_app.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        DataContext dataContext;
+        public PrivacyModel(ILogger<PrivacyModel> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            this.dataContext = dataContext;
+        }
+
+        public void OnGet()
+        {
+            var pizza = new Pizza() { nom = "pizzafest", prix = 5 };
+            dataContext.Pizzas.Add(pizza);
+            dataContext.SaveChanges();
+        }
+    }
+}
